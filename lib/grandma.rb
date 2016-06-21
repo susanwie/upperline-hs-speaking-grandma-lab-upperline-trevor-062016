9753b396eeb77637f570dbf603e90b635515d620
Susan Wie <susanhwie@gmail.com>
2016-06-21 18:07:29 +0000

    Done.

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,2 +1,12 @@
-# Write your code here!
+puts "What are you saying to her?"
+speech = gets.chomp
 
+def speak_to_grandma(speech)
+if speech.upcase == speech
+  return "NO, NOT SINCE 1938!"
+else
+  return "HUH?! SPEAK UP, SONNY!"
+end
+end
+
+puts speak_to_grandma(speech)
